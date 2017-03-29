a60b48fbd559637c75b98c93e692fb663a60afe9
Faustino Aguilar <faustinoaq@gmail.com>
2017-03-29 15:49:19 -0500

    Change Watcher Handler

--- src/kemal-watcher/watcher_handler.cr
+++ src/kemal-watcher/watcher_handler.cr
@@ -3,7 +3,29 @@ require "kemal"
 module Kemal
   class WatcherHandler < Kemal::Handler
     def call(context)
-      context.response.print render "src/kemal-watcher/client_websocket.ecr"
+      context.response.print <<-HTML
+      <!-- Code injected by kemal-watcher -->
+      <script type="text/javascript">
+        // <![CDATA[  <-- For SVG support
+        if ('WebSocket' in window) {
+          var ws = new WebSocket("ws://" + location.host + "/#{WEBSOCKETPATH}");
+          ws.onopen = () => {
+            console.log("Connected to kemal-watcher");
+          };
+          ws.onmessage = (msg) => {
+            if (msg.data == "reload") {
+              window.location.reload();
+            }
+          };
+          ws.onclose = () => {
+            setTimeout(() => {
+              window.location.reload();
+            }, 2000);
+          };
+        }
+        // ]]>
+      </script>\n
+      HTML
       call_next context
     end
   end
